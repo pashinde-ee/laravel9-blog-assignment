5379b66150b4874190e1f3fa469bf49dd59ef7d3
Prashant Shinde <pr.shinde@easternenterprise.com>
2022-02-25 14:22:48 +0530

    Added required packages, seeders, factories, models, controller, etc...

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+use Illuminate\Http\Response;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StorePostRequest $request
+     * @return Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdatePostRequest $request
+     * @param Post $post
+     * @return Response
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Post $post
+     * @return Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    /**
+     * Returns the author of a post.
+     *
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Associated media records.
+     *
+     * @return MorphMany
+     */
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'entity');
+    }
+}
